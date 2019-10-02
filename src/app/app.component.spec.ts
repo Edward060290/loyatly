a9d214d3227c238f674e9fe577327e290de87724
X929208 <Z047474@santander.com.mx>
2019-10-02 02:52:27 -0500

    establesiendo ambienta para pruebas unitarias

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,26 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { PlanComponent } from './plan/plan.component';
+import { ProgrammeComponent } from './programme/programme.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { CardBoardComponent } from './home/card-board/card-board.component';
+import { SlideCardComponent } from './home/slide-card/slide-card.component';
+import { RegisterComponent } from './register/register.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        NavBarComponent,
+        HomeComponent,
+        PlanComponent,
+        ProgrammeComponent,
+        LogInComponent,
+        CardBoardComponent,
+        SlideCardComponent,
+        RegisterComponent
       ],
     }).compileComponents();
   }));
@@ -13,10 +29,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it(`should have as title 'nav'`, async(() => {
+    const fixture = TestBed.createComponent(NavBarComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('nav');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
--- src/test.ts
+++ src/test.ts
@@ -1,14 +1,22 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'zone.js/dist/zone-testing';
+import 'zone.js/dist/long-stack-trace-zone';
+import 'zone.js/dist/proxy.js';
+import 'zone.js/dist/sync-test';
+import 'zone.js/dist/jasmine-patch';
+import 'zone.js/dist/async-test';
+import 'zone.js/dist/fake-async-test';
+
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
-declare const require: any;
+declare const _karma_: any;
 
+declare const require: any;
+_karma_.loaded = function(){};
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
@@ -18,3 +26,5 @@ getTestBed().initTestEnvironment(
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
+
+_karma_.start();
