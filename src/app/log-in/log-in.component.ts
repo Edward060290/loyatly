45778bdca0872337472b471af420f52a61df442e
X929208 <Z047474@santander.com.mx>
2019-10-01 19:13:18 -0500

    se arregla formulario

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-log-in',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogInComponent implements OnInit {
 
-  constructor() { }
+  private logInForm: FormGroup;
+
+  constructor(
+   /*  private fb: FormBuilder */
+  ) { 
+  /*   this.logInForm = this.fb.group({
+      email: ['', [Validators.required, Validators.pattern(this.PATTERN_EMAIL)]],
+      password: ['', Validators.required, Validators.pattern(this.PATTERN_PASSWORD)[]]
+    }) */
+  }
 
   ngOnInit() {
   }
